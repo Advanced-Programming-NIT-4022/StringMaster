71898ca4641a7b2d7549bfef1506e3a5c74915a7
xofError <sadraa772@gmail.com>
2024-02-25 23:44:32 +0330

    All Done !

--- Base Structure/src/main/java/Advanced.java
+++ Base Structure/src/main/java/Advanced.java
@@ -1,3 +1,5 @@
+import javax.naming.Name;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Advanced {
@@ -10,7 +12,19 @@ public class Advanced {
 
      */
     public String wordCensor(String sentence, String word, String newWord){
-        return null;
+        String[] wordArr = new String[sentence.split(" ").length];
+        wordArr = sentence.split(" ");
+
+        int i = 0;
+        for (String eachWord : wordArr) {
+            if(eachWord.equals(word)){
+                wordArr[i] = newWord;
+                break;
+            }
+            i++;
+        }
+        sentence = String.join(" " , wordArr);
+        return sentence;
     }
 
     /**
@@ -20,7 +34,25 @@ public class Advanced {
      * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
      */
     public String normalizingName(String firstName, String lastName){
-        return null;
+
+        firstName = firstName.toLowerCase();
+        lastName = lastName.toLowerCase();
+
+        String[] FirstAlph = new String[firstName.split("").length];
+        String[] LastAlph = new String[lastName.split(("")).length];
+
+        FirstAlph = firstName.split("");
+        LastAlph = lastName.split("");
+
+        FirstAlph[0] = FirstAlph[0].toUpperCase();
+        LastAlph[0] = LastAlph[0].toUpperCase();
+
+        if(lastName.equals(" ") || firstName.equals(" ") || lastName.equals("") || firstName.equals("") ){
+            String Name = String.join("" , FirstAlph) + String.join("" , LastAlph);
+            return Name.trim();
+        }
+            String Name = String.join("" , FirstAlph) + " " + String.join("" , LastAlph);
+            return Name;
     }
 
     /**
@@ -29,7 +61,60 @@ public class Advanced {
      * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
      */
     public String doubleChar(String word) {
-        return null;
+        String[] name = new String[word.split("").length];
+        name = word.split("");
+        int i = 0 , j = 0;
+        for(String each : name){
+            for(j = i+1 ; j<name.length ; j++){
+                if(name[i].equals(name[j])){
+                    name[i] = "";
+                }
+                else{
+                    i++;
+                    break;
+                }
+            }
+        }
+
+
+//        String first = name[0];
+//        String last = name[1];
+//
+//        String[] firstname = new String[first.split("").length];
+//        String[] lastname = new String[last.split("").length];
+//
+//        firstname = first.split("");
+//        lastname = last.split("");
+//
+//
+//
+//        int i = 0;
+//        int j = 0;
+//        for(String eachAl : firstname){
+//
+//            for(j = i+1 ; j<firstname.length ; j++){
+//                if(firstname[i].equals(firstname[j])){
+//                    firstname[j]="";
+//                }
+//            }
+//            i++;
+//        }
+//
+//        i=0;
+//        j=0;
+//
+//        for(String eachAl : lastname){
+//
+//            for(j = i+1 ; j<lastname.length ; j++){
+//                if(lastname[i].equals(lastname[j])){
+//                    lastname[j]="";
+//                }
+//            }
+//            i++;
+//        }
+//        String fullname = String.join("" , firstname)+" "+ String.join("" , lastname);
+//        return fullname ;
+        return String.join("" , name);
     }
 }
 
