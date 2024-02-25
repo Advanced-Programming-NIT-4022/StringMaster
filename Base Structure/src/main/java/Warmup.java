b009b523033e2148a7ed15e2da933fa1fa97535d
xofError <sadraa772@gmail.com>
2024-02-25 19:11:04 +0330

    Warmup Done !!!

--- Base Structure/src/main/java/Warmup.java
+++ Base Structure/src/main/java/Warmup.java
@@ -22,7 +22,31 @@ public class Warmup {
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
      */
     public int oddEvenCounter(String number, boolean searchForEven) {
-        return -1;
+
+        String[] strArr = number.split("");
+
+        int[] numArr = new int[strArr.length];
+
+        for(int i = 0 ; i < numArr.length ; i++){
+
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        int counter = 0;
+        for (int eachNumber : numArr) {
+
+            if (searchForEven) {
+                if( eachNumber%2 == 0){
+                    counter++;
+                }
+            }
+            else{
+                if(eachNumber%2 == 1){
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 
     /**
@@ -31,6 +55,23 @@ public class Warmup {
      * @return The word that is first in alphabet column
      */
     public String firstWord(String wordA, String wordB) {
+        char[] word1 = wordA.toCharArray();
+        char[] word2 = wordB.toCharArray();
+        int MinimumLen = Math.min(word1.length, word2.length);
+
+        int i = 0;
+
+        while (i < MinimumLen) {
+
+            if (word1[i] < word2[i]) {
+                return wordA;
+            } else if (word2[i] < word1[i]) {
+                return wordB;
+            } else {
+                i++;
+                continue;
+            }
+        }
         return null;
     }
 }
