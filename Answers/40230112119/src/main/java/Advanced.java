184078a84a000f8f1477473668100860305f8c74
avrillles <mmzonoz@gmail.com>
2024-03-03 14:58:42 +0330

    first method

---
+++ Answers/40230112119/src/main/java/Advanced.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Advanced {
+
+    /**
+     * Goal : Changing a Sentence Content
+
+     * In this function, you have a sentence, a word & a newWord as Entry
+     * You have to search the sentence to find the word that you were given as input and change it with the newWord
+
+     */
+    public String wordCensor(String sentence, String word, String newWord){
+        return null;
+    }
+
+    /**
+     * In this function You have a firstName and a lastName as Entry and you have to normalize them as a fullName
+     * @param firstName is a first name with irregular letters (example : hARry)
+     * @param lastName is a last name with irregular letters (example : pOtTeR)
+     * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
+     */
+    public String normalizingName(String firstName, String lastName){
+        return null;
+    }
+
+    /**
+     * Removing repeated letter in a word
+     * @param word This input could have Consecutive repeated letters or not
+     * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
+     */
+    public String doubleChar(String word) {
+        return null;
+    }
+}
+
